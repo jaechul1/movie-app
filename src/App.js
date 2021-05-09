fc3d2a0e4cc9cfd8e0fc4ee21d3f21122153f740
Jaechul Lee <jaechul10@naver.com>
2021-05-09 19:07:19 +0900

    following lectures ~ #4.5

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
-import PropTypes from "prop-types";
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-  add = () => {
-    this.setState((current) => {
-      current.count += 1;
-      return current;
+  async getMovies() {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=download_count&minimum_rating=7&genre=fantasy"
+    );
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  truncate = (str, last) =>
+    str.length < last
+      ? str
+      : `${str.substr(0, str.substr(0, last).lastIndexOf(" "))}...`;
+  renderMovies(movies) {
+    return movies.map((movie) => {
+      return (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          year={movie.year}
+          title={movie.title}
+          summary={this.truncate(movie.summary, 200)}
+          poster={movie.medium_cover_image}
+          genres={movie.genres}
+        />
+      );
     });
-  };
-  minus = () => {
-    this.setState((current) => {
-      current.count -= 1;
-      return current;
-    });
-  };
+  }
   render() {
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading</span>
+          </div>
+        ) : (
+          <div className="movies">{this.renderMovies(movies)}</div>
+        )}
+      </section>
     );
   }
 }
